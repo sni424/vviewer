25a1e5a3f3899db954a24603e629c946bb83711e
OpMAY <hjw8622@gmail.com>
2025-04-10 21:37:31 +0900

    update sharpen shader

--- src/components/canvas/Sharpen.tsx
+++ src/components/canvas/Sharpen.tsx
@@ -18,30 +18,37 @@ const sharpenShader = {
     void main() {
       vUv = uv;
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      // 하위 코드는 Quad 적용 용
+      //vUv = position.xy*0.5+0.5; 
+      //gl_Position = vec4(position.xy, 1.0, 1.0);
     }
   `,
   fragmentShader: `
-    uniform sampler2D tDiffuse;
+    #define GAMMA_FACTOR 2.200000047683716
+    uniform mediump sampler2D tDiffuse;
     uniform vec2 resolution;
     uniform float strength;
     varying vec2 vUv;
+    
+    vec4 LinearToGamma(vec4 value, float gammaFactor ) {
+      return vec4( pow( value.rgb, vec3( 1.0 / gammaFactor ) ), value.a );
+    }
+    
+    vec4 linearToOutputTexel2( vec4 value ) {
+      return LinearToGamma( value, float( GAMMA_FACTOR ) );
+    }
 
     void main() {
       vec4 original = texture2D(tDiffuse, vUv);
-      vec2 texelSize = 1.0 / resolution;
-      vec4 blur = vec4(0.0);
-      blur += texture2D(tDiffuse, vUv + vec2(-texelSize.x, -texelSize.y));
-      blur += texture2D(tDiffuse, vUv + vec2(0.0, -texelSize.y));
-      blur += texture2D(tDiffuse, vUv + vec2(texelSize.x, -texelSize.y));
-      blur += texture2D(tDiffuse, vUv + vec2(-texelSize.x, 0.0));
-      blur += texture2D(tDiffuse, vUv);
-      blur += texture2D(tDiffuse, vUv + vec2(texelSize.x, 0.0));
-      blur += texture2D(tDiffuse, vUv + vec2(-texelSize.x, texelSize.y));
-      blur += texture2D(tDiffuse, vUv + vec2(0.0, texelSize.y));
-      blur += texture2D(tDiffuse, vUv + vec2(texelSize.x, texelSize.y));
-      blur /= 9.0;
-      vec4 difference = original - blur;
-      gl_FragColor = original + strength * difference;
+      vec2 fragCoord = vUv * resolution;
+      float neighbor = strength * -1.0;
+      float center = strength * 4.0 + 1.0;
+      vec3 color = original.rgb * center
+      + texture2D(tDiffuse, (fragCoord + vec2( 1, 0)) / resolution).rgb * neighbor
+      + texture2D(tDiffuse, (fragCoord + vec2(-1, 0)) / resolution).rgb * neighbor
+      + texture2D(tDiffuse, (fragCoord + vec2( 0, 1)) / resolution).rgb * neighbor
+      + texture2D(tDiffuse, (fragCoord + vec2( 0, -1)) / resolution).rgb * neighbor;
+      gl_FragColor = linearToOutputTexel2(mix(original, vec4(color, original.a), 1.0));
     }
   `,
 };
